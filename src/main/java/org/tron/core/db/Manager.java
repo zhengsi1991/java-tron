74f62a6e4c9b68a0c9ac64005a313860e20569b3
jiangyy0824 <jiangyy0824@163.com>
2018-12-12 20:49:02 +0800

    construct local witness environment

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -1644,4 +1644,23 @@ public class Manager {
     revokingStore.setMode(mode);
   }
 
+  public void insertWitness(byte[] keyAddress, int idx) {
+    ByteString address = ByteString.copyFrom(keyAddress);
+
+    final AccountCapsule accountCapsule;
+    if (!this.accountStore.has(keyAddress)) {
+      accountCapsule = new AccountCapsule(ByteString.EMPTY,
+              address, AccountType.AssetIssue, 0L);
+    } else {
+      accountCapsule = this.accountStore.getUnchecked(keyAddress);
+    }
+    accountCapsule.setIsWitness(true);
+    this.accountStore.put(keyAddress, accountCapsule);
+
+    final WitnessCapsule witnessCapsule =
+            new WitnessCapsule(address, 1000000000, "mock_witness_" + idx);
+    witnessCapsule.setIsJobs(true);
+    this.witnessStore.put(keyAddress, witnessCapsule);
+  }
+
 }
