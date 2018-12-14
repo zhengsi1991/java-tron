483ab3af4f4ce5608b142caa8470f9745858e351
zhoujialiang <zhoujialiang@tron.network>
2018-12-14 12:30:57 +0800

    fix event bug

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -51,6 +51,7 @@ import org.tron.abi.datatypes.Type;
 import org.tron.abi.datatypes.generated.AbiTypes;
 import org.tron.common.overlay.discover.node.Node;
 import org.tron.common.runtime.config.VMConfig;
+import org.tron.common.runtime.utils.MUtil;
 import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.ForkController;
 import org.tron.common.utils.SessionOptional;
@@ -1117,19 +1118,18 @@ public class Manager {
       return;
     }
     try {
-      Protocol.SmartContract.ABI abi = abiCache.getIfPresent(contractAddress);
-      if (abi == null) {
-        abi = getContractStore().getABI(contractAddress);
-        if (abi == null) {
-          return;
-        }
-        abiCache.put(contractAddress, abi);
-      }
-      Protocol.SmartContract.ABI finalAbi = abi;
-
       IntStream.range(0,logList.size()).forEach(idx -> {
         org.tron.protos.Protocol.TransactionInfo.Log log = logList.get(idx);
-        finalAbi.getEntrysList().forEach(abiEntry -> {
+        byte[] logContractAddress = MUtil.convertToTronAddress(log.getAddress().toByteArray());
+        Protocol.SmartContract.ABI abi = abiCache.getIfPresent(logContractAddress);
+        if (abi == null) {
+          abi = getContractStore().getABI(logContractAddress);
+          if (abi == null) {
+            return;
+          }
+          abiCache.put(logContractAddress, abi);
+        }
+        abi.getEntrysList().forEach(abiEntry -> {
           if (abiEntry.getType() != Protocol.SmartContract.ABI.Entry.EntryType.Event) {
             return;
           }
@@ -1199,6 +1199,7 @@ public class Manager {
 
           rawJsonObject.put("topics", rawTopicsJsonArray);
           rawJsonObject.put("data", rawLogData);
+          rawJsonObject.put("contract", Hex.toHexString(logContractAddress));
 
           long blockNumber = block.getBlockHeader().getRawData().getNumber();
           long blockTimestamp = block.getBlockHeader().getRawData().getTimestamp();
