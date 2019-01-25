89da33f200b070ff1afefe506b9ef553ebb35c56
HuangZhengSi <huangzhengsi@tron.network>
2019-01-25 20:01:26 +0800

    update

---
+++ src/test/java/stest/tron/wallet/account/AssetIssueAndCreateExchange.java
@@ -0,0 +1,125 @@
+package stest.tron.wallet.account;
+
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.WalletGrpc;
+import org.tron.core.Wallet;
+import stest.tron.wallet.common.client.Parameter.CommonConstant;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+import java.util.concurrent.TimeUnit;
+import com.google.protobuf.ByteString;
+import org.tron.protos.Protocol.Account;
+
+
+@Slf4j
+public class AssetIssueAndCreateExchange {
+    //testng001、testng002、testng003、testng004
+    //fromAssetIssue
+    private final String testKey001 =
+            "0528dc17428585fc4dece68b79fa7912270a1fe8e85f244372f59eb7e8925e04";
+    //toAssetIssue
+    private final String testKey002 =
+            "553c7b0dee17d3f5b334925f5a90fe99fb0b93d47073d69ec33eead8459d171e";
+
+    //Default
+    private final String defaultKey =
+            "549c7797b351e48ab1c6bb5857138b418012d97526fc2acba022357d49c93ac0";
+
+    private final byte[] fromAddress = PublicMethed.getFinalAddress(testKey001);
+
+    private final byte[] defaultAddress = PublicMethed.getFinalAddress(defaultKey);
+    ByteString assetAccountId1;
+    ByteString assetAccountId2;
+    Long firstTokenInitialBalance = 50000000000000L;
+    Long secondTokenInitialBalance = 50000000000000L;
+
+    private Long start;
+    private Long end;
+
+    private ManagedChannel channelFull = null;
+    private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+
+    private String fullnode = "47.94.239.172:50051";
+
+
+    @BeforeSuite
+    public void beforeSuite() {
+        Wallet wallet = new Wallet();
+        Wallet.setAddressPreFixByte(CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+    }
+
+    /**
+     * constructor.
+     */
+
+    @BeforeClass(enabled = true)
+    public void beforeClass() {
+
+        channelFull = ManagedChannelBuilder.forTarget(fullnode)
+                .usePlaintext(true)
+                .build();
+        blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+    }
+
+    @Test(enabled = true)
+    public void continueRun() {
+        Account fromAccount = PublicMethed.queryAccount(testKey001,blockingStubFull);
+        Account toAccount   = PublicMethed.queryAccount(defaultKey,blockingStubFull);
+
+        PublicMethed.sendcoin(fromAddress,10000000000000L,defaultAddress,defaultKey,blockingStubFull);
+        logger.info("sendcoin");
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+        if(toAccount.getAssetCount()==0)
+        {
+            start = System.currentTimeMillis() + 2000;
+            end = System.currentTimeMillis() + 1000000000;
+            PublicMethed.createAssetIssue(defaultAddress, "xxd", 50000000000000L,
+                    1, 1, start, end, 1, "wwwwww", "wwwwwwww", 100000L,
+                    100000L, 1L, 1L, defaultKey, blockingStubFull);
+            logger.info("createAssetIssue");
+        }
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+        Account getAssetIdFromThisAccount;
+        getAssetIdFromThisAccount =PublicMethed.queryAccount(defaultAddress,blockingStubFull);
+        assetAccountId1 =getAssetIdFromThisAccount.getAssetIssuedID();
+
+        String trx = "_";
+        byte[] b = trx.getBytes();
+        PublicMethed.exchangeCreate(assetAccountId1.toByteArray(),firstTokenInitialBalance,
+                b,secondTokenInitialBalance,defaultAddress,
+                defaultKey,blockingStubFull);
+        logger.info("exchangeCreate");
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+        PublicMethed.waitProduceNextBlock(blockingStubFull);
+    }
+
+
+    /**
+     * constructor.
+     */
+
+    @AfterClass(enabled = true)
+    public void shutdown() throws InterruptedException {
+        if (channelFull != null) {
+            channelFull.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        }
+
+    }
+}
+
