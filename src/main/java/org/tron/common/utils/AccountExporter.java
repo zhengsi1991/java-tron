d440ecc7d1e23fe69383760c02a839947e20958b
Weiyu Xue <wxue2012@gmail.com>
2019-02-08 20:43:58 -0800

    add 4 kinds of frozen trx; add export normal, contract, asset, and missing accounts;

--- src/main/java/org/tron/common/utils/AccountExporter.java
+++ src/main/java/org/tron/common/utils/AccountExporter.java
@@ -22,51 +22,174 @@ import org.tron.core.Wallet;
 import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.db.BlockStore;
-import org.tron.core.db.Manager;
 import org.tron.core.services.interfaceOnSolidity.WalletOnSolidity;
+import org.tron.protos.Protocol.AccountType;
 
 @Slf4j
 @Component
 public class AccountExporter {
-  private static final String FILE_NAME = "accounts.csv";
+  public static final String FILE_NAME = "accounts.csv";
   public static final AtomicLong EXPORT_NUM = new AtomicLong(0);
   private static WalletOnSolidity walletOnSolidity;
-  private static Manager manager;
 
   @Autowired
   public void setWalletOnSolidity(WalletOnSolidity walletOnSolidity) {
     AccountExporter.walletOnSolidity = walletOnSolidity;
   }
 
-  @Autowired
-  public void setManager(Manager manager) {
-    AccountExporter.manager = manager;
-  }
-
-  public static void export() {
+  public static void export(BlockStore blockStore, Iterable<Entry<byte[], AccountCapsule>> iterable) {
     walletOnSolidity.futureGetWithoutTimeout(() -> {
-      List<BlockCapsule> blockList = manager.getBlockStore().getBlockByLatestNum(1);
+      List<BlockCapsule> blockList = blockStore.getBlockByLatestNum(1);
       if (CollectionUtils.isNotEmpty(blockList) && blockList.get(0).getNum() == EXPORT_NUM.get()) {
-        exportAccount();
+        exportAll(iterable);
+        exportNormal(iterable);
+        exportContract(iterable);
+        exportAssetIssue(iterable);
+        exportMissing(iterable);
       }
     });
   }
 
-  private static void exportAccount() {
+  private static void exportAll(Iterable<Entry<byte[], AccountCapsule>> iterable) {
     AtomicLong total = new AtomicLong(0);
-    Map<String, Long> accounts = Streams.stream(manager.getAccountStore())
-        .filter(e -> !manager.getContractStore().has(e.getKey()))
-        .map(e -> Maps.immutableEntry(
-            Wallet.encode58Check(e.getKey()),
-            e.getValue().getBalance()
-                + e.getValue().getFrozenBalance()
-                + e.getValue().getEnergyFrozenBalance()))
+    Map<String, Long> accounts = Streams.stream(iterable)
+        .map(e -> Maps.immutableEntry(Wallet.encode58Check(e.getKey()), e.getValue().getBalance() + 
+                                                                        e.getValue().getFrozenBalance() +
+                                                                        e.getValue().getEnergyFrozenBalance() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy()))
         .peek(e -> {
           if (e.getValue() >= 0) total.getAndAdd(e.getValue());
         })
         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1));
 
-    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_" + FILE_NAME),
+    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_all_" + FILE_NAME),
+        CSVFormat.EXCEL.withHeader("address", "balance"))) {
+      printer.printRecord("total", total.get());
+      accounts.forEach((k, v) -> {
+            String address = k;
+            long balance = v;
+            try {
+              printer.printRecord(address, balance);
+            } catch (Exception e1) {
+              logger.error("address {} write error.", address);
+            }
+          });
+    } catch (Exception e) {
+      logger.error("export error", e);
+    }
+  }
+
+  private static void exportNormal(Iterable<Entry<byte[], AccountCapsule>> iterable) {
+    AtomicLong total = new AtomicLong(0);
+    Map<String, Long> accounts = Streams.stream(iterable)
+        .filter(e -> e.getValue().getType() == AccountType.Normal)   // 0: Normal
+        .map(e -> Maps.immutableEntry(Wallet.encode58Check(e.getKey()), e.getValue().getBalance() + 
+                                                                        e.getValue().getFrozenBalance() +
+                                                                        e.getValue().getEnergyFrozenBalance() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy()))
+        .peek(e -> {
+          if (e.getValue() >= 0) total.getAndAdd(e.getValue());
+        })
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1));
+
+    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_normal_" + FILE_NAME),
+        CSVFormat.EXCEL.withHeader("address", "balance"))) {
+      printer.printRecord("total", total.get());
+      accounts.forEach((k, v) -> {
+            String address = k;
+            long balance = v;
+            try {
+              printer.printRecord(address, balance);
+            } catch (Exception e1) {
+              logger.error("address {} write error.", address);
+            }
+          });
+    } catch (Exception e) {
+      logger.error("export error", e);
+    }
+  }
+
+  private static void exportContract(Iterable<Entry<byte[], AccountCapsule>> iterable) {
+    AtomicLong total = new AtomicLong(0);
+    Map<String, Long> accounts = Streams.stream(iterable)
+        .filter(e -> e.getValue().getType() == AccountType.Contract)   // 2: Contract
+        .map(e -> Maps.immutableEntry(Wallet.encode58Check(e.getKey()), e.getValue().getBalance() + 
+                                                                        e.getValue().getFrozenBalance() +
+                                                                        e.getValue().getEnergyFrozenBalance() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy()))
+        .peek(e -> {
+          if (e.getValue() >= 0) total.getAndAdd(e.getValue());
+        })
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1));
+
+    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_contract_" + FILE_NAME),
+        CSVFormat.EXCEL.withHeader("address", "balance"))) {
+      printer.printRecord("total", total.get());
+      accounts.forEach((k, v) -> {
+            String address = k;
+            long balance = v;
+            try {
+              printer.printRecord(address, balance);
+            } catch (Exception e1) {
+              logger.error("address {} write error.", address);
+            }
+          });
+    } catch (Exception e) {
+      logger.error("export error", e);
+    }
+  }
+
+  private static void exportAssetIssue(Iterable<Entry<byte[], AccountCapsule>> iterable) {
+    AtomicLong total = new AtomicLong(0);
+    Map<String, Long> accounts = Streams.stream(iterable)
+        .filter(e -> e.getValue().getType() == AccountType.AssetIssue)   // 1: AssetIssue
+        .map(e -> Maps.immutableEntry(Wallet.encode58Check(e.getKey()), e.getValue().getBalance() + 
+                                                                        e.getValue().getFrozenBalance() +
+                                                                        e.getValue().getEnergyFrozenBalance() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy()))
+        .peek(e -> {
+          if (e.getValue() >= 0) total.getAndAdd(e.getValue());
+        })
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1));
+
+    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_assetissue_" + FILE_NAME),
+        CSVFormat.EXCEL.withHeader("address", "balance"))) {
+      printer.printRecord("total", total.get());
+      accounts.forEach((k, v) -> {
+            String address = k;
+            long balance = v;
+            try {
+              printer.printRecord(address, balance);
+            } catch (Exception e1) {
+              logger.error("address {} write error.", address);
+            }
+          });
+    } catch (Exception e) {
+      logger.error("export error", e);
+    }
+  }
+
+  private static void exportMissing(Iterable<Entry<byte[], AccountCapsule>> iterable) {
+    AtomicLong total = new AtomicLong(0);
+    Map<String, Long> accounts = Streams.stream(iterable)
+        .filter(e -> (e.getValue().getType() != AccountType.AssetIssue) &&
+                     (e.getValue().getType() != AccountType.Contract) && 
+                     (e.getValue().getType() != AccountType.Normal))   // ?
+        .map(e -> Maps.immutableEntry(Wallet.encode58Check(e.getKey()), e.getValue().getBalance() + 
+                                                                        e.getValue().getFrozenBalance() +
+                                                                        e.getValue().getEnergyFrozenBalance() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy() +
+                                                                        e.getValue().getDelegatedFrozenBalanceForEnergy()))
+        .peek(e -> {
+          if (e.getValue() >= 0) total.getAndAdd(e.getValue());
+        })
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1));
+
+    try (CSVPrinter printer = new CSVPrinter(new FileWriter("block_" + EXPORT_NUM.get() + "_missing_" + FILE_NAME),
         CSVFormat.EXCEL.withHeader("address", "balance"))) {
       printer.printRecord("total", total.get());
       accounts.forEach((k, v) -> {
