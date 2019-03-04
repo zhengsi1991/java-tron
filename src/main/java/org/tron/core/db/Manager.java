3c25e155ac7e727d7d31f997dc0bdca9bb0dbd52
wubin1 <wubin1@tron.network>
2019-03-04 20:01:30 +0800

    get transaction info by id

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -1397,7 +1397,7 @@ public class Manager {
 
       // total process time of deferred transactions should not exceeds the maxDeferredTransactionProcessTime
       if (trx.getContractType() == Constant.UNEXECUTEDDEFERREDTRANSACTION){
-        if (totalDeferredTransactionProcessTime >= getDynamicPropertiesStore().getMaxDeferredTransactionProcessTime()){
+        if (totalDeferredTransactionProcessTime >= 100){
           logger.info("totalDeferredTransactionProcessTime {}, exceeds {}", totalDeferredTransactionProcessTime, getDynamicPropertiesStore().getMaxDeferredTransactionProcessTime());
           postponedTrxCount++;
           postponedDeferredTrxCount++;
