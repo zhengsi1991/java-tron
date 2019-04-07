e7a925d349cab02544671225378630504806e80c
wubin1 <wubin1@tron.network>
2019-04-07 11:28:55 +0800

    caculate vote

--- src/main/java/org/tron/common/application/ApplicationImpl.java
+++ src/main/java/org/tron/common/application/ApplicationImpl.java
@@ -1,13 +1,23 @@
 package org.tron.common.application;
 
+import com.google.common.collect.Streams;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.common.logsfilter.EventPluginLoader;
+import org.tron.common.utils.ByteArray;
+import org.tron.core.capsule.VotesCapsule;
 import org.tron.core.config.args.Args;
 import org.tron.core.db.BlockStore;
 import org.tron.core.db.Manager;
 import org.tron.core.net.TronNetService;
+import org.tron.protos.Protocol.Vote;
 
 @Slf4j(topic = "app")
 @Component
@@ -55,8 +65,37 @@ public class ApplicationImpl implements Application {
 
   @Override
   public void shutdown() {
+    List<Entry<byte[], VotesCapsule>> list = Streams.stream(dbManager.getVotesStore()).collect(Collectors.toList());
+    Map<String, Long> hmap = new HashMap<>();
+    for (Entry<byte[], VotesCapsule> v : list) {
+      for (Vote p :v.getValue().getNewVotes()) {
+        String address = ByteArray.toHexString(p.getVoteAddress().toByteArray());
+        if (hmap.containsKey(address)) {
+          hmap.put(address, new Long(0));
+        }
+        hmap.put(address, hmap.get(address) + p.getVoteCount());
+      }
+
+      for (Vote p :v.getValue().getOldVotes()) {
+        String address = ByteArray.toHexString(p.getVoteAddress().toByteArray());
+        if (hmap.containsKey(address)) {
+          hmap.put(address, new Long(0));
+        }
+        hmap.put(address, hmap.get(address) - p.getVoteCount());
+      }
+    }
+
+    logger.info("******** result ********");
+
+    Streams.stream(hmap.entrySet()).sorted(Comparator.comparing(Entry::getValue)).forEach(
+        stringLongEntry -> {
+          logger.info("address: " + stringLongEntry.getKey() + "votes: " + stringLongEntry.getValue());
+        }
+    );
+
     logger.info("******** begin to shutdown ********");
-    tronNetService.close();
+
+     tronNetService.close();
     synchronized (dbManager.getRevokingStore()) {
       closeRevokingStore();
       closeAllStore();
